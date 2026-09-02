bdcdd3d609b21019d04311a9bfff64fde20bdee2
Russel Jay Calamba <calambarusseljay5@gmail.com>
2026-09-02 12:25:18 +0800

    fix(modal): set photo confirmation dialog z-index to 9999999 to render in front of My Account modal

--- src/components/modals/MyAccountModal.jsx
+++ src/components/modals/MyAccountModal.jsx
@@ -861,7 +861,7 @@ const MyAccountModal = ({ isOpen, onClose }) => {
         createPortal(
           <AnimatePresence>
             {photoConfirmModal.isOpen && (
-              <div className="fixed inset-0 z-[100000] flex items-center justify-center p-4">
+              <div className="fixed inset-0 z-[9999999] flex items-center justify-center p-4">
                 <motion.div
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
